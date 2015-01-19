324df5ccf52cc65a8fd3596e27757e17221f6cdd
Mathieu Gordon <mathieu.gordon@gmail.com>
2015-01-19 14:56:29 +0000

    adds scopes

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,9 @@ class Order < ActiveRecord::Base
   belongs_to :meal
   has_one :review
 
+  scope :current, -> { where("fetch_at > ?", Time.zone.now - 1.hour)}
+  scope :archived, -> { where("fetch_at < ?", Time.zone.now - 1.hour)}
+
   validates :user_id, :meal_id, :price, :quantity, :fetch_at, :credit_card_number, :credit_card_name, :credit_card_expiry, presence: true
   # validates :quantity <= self.meal.quantity
   # validates :price == self.meal.price
