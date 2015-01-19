490850ef00776a4a1f368b4612cc8f3243fde86d
Mathieu Gordon <mathieu.gordon@gmail.com>
2015-01-19 14:45:50 +0000

    updates cancan abilities

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'haml-rails'
 gem 'neat'
 gem 'devise'
+gem 'cancancan'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
---
+++ app/models/ability.rb
@@ -0,0 +1,77 @@
+class Ability
+    include CanCan::Ability
+
+    def initialize(user)
+        user ||= User.new
+        if user.role? :admin
+
+            can :manage, :all
+
+        elsif user.role? :chef
+
+            # can read basics, as per anonymous user
+            can :read, [Category, Meal, Review]
+            can :read, User, role: "chef"
+
+            # can read own orders, both those placed and those received
+            can :read, Order do |order|
+                order.user.id == user.id || order.meal.user.id == user.id
+            end
+            # can place order, but not for own meal (NOT WORKING)
+            can :create, Order do |order|
+                order.meal.user.id != user.id
+            end
+
+            # can create, update and destroy reviews on own orders
+            can [:create, :update, :destroy], Review do |review|
+                review.order.user.id == user.id
+            end
+
+            # can update chef profile
+            can :update, User, id: user.id
+
+            # can create meal
+            can :create, Meal
+
+            # can update own meals
+            can :update, Meal, user_id: user.id
+
+            # can destroy own meal but only if no dependents
+            can :destroy, Meal do |meal|
+                meal.user.id == user.id && meal.orders.count == 0
+            end
+
+        elsif user.persisted?
+
+            # can read basics, as per anonymous user
+            can :read, [Category, Meal, Review]
+            can :read, User, role: "chef"
+
+            # can read own orders
+            can :read, Order do |order|
+                order.user.id == user.id
+            end
+
+            # can place order if the meal is available and current (NOT WORKING)
+            can :create, Order do |order|
+                order.meal.available? && order.meal.current?
+            end
+
+            # can create, update and destroy reviews on own orders
+            can [:create, :update, :destroy], Review do |review|
+                review.order.user.id == user.id
+            end
+
+            # can "create" (but really "update") chef profile
+            can :update, User, id: user.id
+
+        else # anonymous user
+
+            # can read basics
+            can :read, [Category, Meal, Review]
+            can :read, User, role: "chef"
+        
+        end
+    end
+
+end
