db05036de63ddb17bae14dff50d72d2a2fa066a2
Mathieu Gordon <mathieu.gordon@gmail.com>
2015-01-19 17:44:15 +0000

    corrects error in review validations - should require order_id not user_id

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :order
 
-  validates :rating, :user_id, presence: true
+  validates :rating, :order_id, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
 end
