39d086c00caee7d355454518088367530e8b8c0c
Mathieu Gordon <mathieu.gordon@gmail.com>
2015-01-19 15:02:47 +0000

    adds seed users and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.delete_all
+
+categories = %w(Indian Chinese Vietnamese Italian Thai Japanese French American)
+categories.each do |category|
+  Category.create!(name: category)
+end
+
+User.delete_all
+
+User.create!(email: "admin@mailinator.com", password: "password", first_name: "Admin", last_name: "Guy", role: "admin")
+User.create!(email: "nina@mailinator.com", password: "password", first_name: "Nina", last_name: "Regli")
+User.create!(email: "julia@mailinator.com", password: "password", first_name: "Julia", last_name: "Mansson")
+User.create!(email: "mat@mailinator.com", password: "password", first_name: "Mathieu", last_name: "Gordon")
+User.create!(email: "toni@mailinator.com", password: "password", first_name: "Toni", last_name: "Goncalves", role: "chef", chef_name: "Chef Eusebio", description: "best chef", phone_number: "07899 123 456", address_line_1: "68 Hanbury Street", city: "London", postcode: "E1 5JL", country:"United Kingdom")
+User.create!(email: "bob@mailinator.com", password: "password", first_name: "Bob", last_name: "Bobson",role: "chef", chef_name: "Chef Bob", description: "good chef", phone_number: "07777 888 999", address_line_1: "52 Lofting Road", address_line_2: "Islington", city: "London", postcode: "N1 1ET", country:"United Kingdom")
+User.create!(email: "dirk@mailinator.com", password: "password", first_name: "Dirk", last_name: "Dirkson", role: "chef", chef_name: "Chef Dirk", description: "chef in training", phone_number: "07890 123 456", address_line_1: "2500 Great Western Road", city: "Glasgow", postcode: "G15 6RW", country:"United Kingdom")
+# User.create!(email: "@mailinator.com", password: "password", first_name: "", last_name: "", role: "chef", chef_name: "", description: "", phone_number: "", address_line_1: "", address_line_2: "", city: "", postcode: "", country:"United Kingdom")
