21ecc245b2d48a715b76601d06ed38adb25dcae5
Mathieu Gordon <mathieu.gordon@gmail.com>
2015-01-20 14:47:52 +0000

    adds facebook login as alternative to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119131501) do
+ActiveRecord::Schema.define(version: 20150120143307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,8 @@ ActiveRecord::Schema.define(version: 20150119131501) do
     t.float    "latitude"
     t.float    "longitude"
     t.float    "average_rating"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
